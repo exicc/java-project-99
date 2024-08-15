661a797261d385f87112587222e98551c1227703
Exicc <ping.u@ya.ru>
2024-08-15 13:12:51 +0300

    Test fixes

--- app/src/test/java/hexlet/code/app/AppApplicationTests.java
+++ app/src/test/java/hexlet/code/app/AppApplicationTests.java
@@ -1,7 +1,15 @@
 package hexlet.code.app;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertThat(true).isTrue();
+    }
+
 }
