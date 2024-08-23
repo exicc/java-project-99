6d63925f5ad5d05001acfa70d339a4567b7bf701
Exicc <ping.u@ya.ru>
2024-08-23 21:37:35 +0300

    Mv tests and add some controller tests

---
+++ app/src/test/java/hexlet/code/app/controller/api/AppApplicationTests.java
@@ -0,0 +1,137 @@
+package hexlet.code.app.controller.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.app.dto.UserDTO;
+import hexlet.code.app.dto.UserUpdateDTO;
+import hexlet.code.app.mapper.UserMapper;
+import hexlet.code.app.model.User;
+import hexlet.code.app.repository.UserRepository;
+import net.datafaker.Faker;
+import org.instancio.Instancio;
+import org.instancio.Select;
+import org.junit.jupiter.api.Test;
+import org.openapitools.jackson.nullable.JsonNullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.BeforeEach;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AppApplicationTests {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private Faker faker;
+
+    private User testUser;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @BeforeEach
+    public void setUp() {
+        testUser = Instancio.of(User.class)
+                .ignore(Select.field(User::getId))
+                .supply(Select.field(User::getFirstName), () -> faker.name().firstName())
+                .supply(Select.field(User::getLastName), () -> faker.name().lastName())
+                .supply(Select.field(User::getEmail), () -> faker.internet().emailAddress())
+                .create();
+
+
+    }
+
+    @Test
+    public void testWelcome() throws Exception {
+        userRepository.save(testUser);
+
+        mockMvc.perform(get("/welcome"))
+            .andExpect(status()
+                    .isOk())
+            .andExpect(content().string("Welcome to Spring"))
+            .andReturn();
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        var dto = userMapper.map(testUser);
+
+        var request = post("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto));
+
+        var result = mockMvc.perform(request)
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        var responseBody = result.getResponse().getContentAsString();
+        var createdUserDto = objectMapper.readValue(responseBody, UserDTO.class);
+
+        var user = userRepository.findById(createdUserDto.getId()).orElse(null);
+
+        assertThat(user).isNotNull();
+        assertThat(user.getEmail()).isEqualTo(dto.getEmail());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        userRepository.save(testUser);
+
+        var dto = new UserUpdateDTO();
+        dto.setEmail(JsonNullable.of(faker.internet().emailAddress()));
+
+        var request = put("/api/users/" + testUser.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto));
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk());
+
+        var user = userRepository.findById(testUser.getId()).get();
+        var expectedEmail = dto.getEmail().orElse(null);
+        assertThat(user.getEmail()).isEqualTo(expectedEmail);
+    }
+
+
+    @Test
+    public void testShow() throws Exception {
+        userRepository.save(testUser);
+
+        var request = get("/api/users/" + testUser.getId());
+        var result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+        var body = result.getResponse().getContentAsString();
+        var expectedJson = objectMapper.writeValueAsString(userMapper.map(testUser));
+        assertThat(body).isEqualTo(expectedJson);
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        userRepository.save(testUser);
+        var request = get("/api/users/" + testUser.getId());
+        mockMvc.perform(request)
+                .andExpect(status().isNoContent());
+
+        assertThat(userRepository.existsById(testUser.getId())).isFalse();
+    }
+}
