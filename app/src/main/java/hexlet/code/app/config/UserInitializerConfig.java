76c455391b664856ba9585f094b155625ac088d7
Exicc <ping.u@ya.ru>
2024-08-27 14:45:30 +0300

    Add default user init

---
+++ app/src/main/java/hexlet/code/app/config/UserInitializerConfig.java
@@ -0,0 +1,37 @@
+package hexlet.code.app.config;
+
+import hexlet.code.app.model.User;
+import hexlet.code.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UserInitializerConfig {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public UserInitializerConfig(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public CommandLineRunner initializeUser() {
+        return args -> {
+            String email = "hexlet@example.com";
+            if (!userRepository.existsByEmail(email)) {
+                User user = new User();
+                user.setEmail(email);
+                user.setPassword(passwordEncoder.encode("qwerty"));
+                userRepository.save(user);
+            }
+        };
+    }
+}
