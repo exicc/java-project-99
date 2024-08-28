afc618749ae4bd04529204114ecf828cece7ad29
Exicc <ping.u@ya.ru>
2024-08-28 15:25:03 +0300

    Rework tests for auth

--- app/src/test/java/hexlet/code/app/controller/api/AppApplicationTests.java
+++ app/src/test/java/hexlet/code/app/controller/api/AppApplicationTests.java
@@ -1,6 +1,7 @@
 package hexlet.code.app.controller.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.app.util.ModelGenerator;
 import hexlet.code.app.dto.UserDTO;
 import hexlet.code.app.dto.UserUpdateDTO;
 import hexlet.code.app.mapper.UserMapper;
@@ -8,19 +9,26 @@ import hexlet.code.app.model.User;
 import hexlet.code.app.repository.UserRepository;
 import net.datafaker.Faker;
 import org.instancio.Instancio;
-import org.instancio.Select;
 import org.junit.jupiter.api.Test;
 import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.web.servlet.request
+        .SecurityMockMvcRequestPostProcessors
+        .JwtRequestPostProcessor;
 import org.springframework.test.web.servlet.MockMvc;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+
+import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -34,6 +42,9 @@ import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 class AppApplicationTests {
 
     @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
     private ObjectMapper objectMapper;
 
     @Autowired
@@ -45,31 +56,34 @@ class AppApplicationTests {
     @Autowired
     private Faker faker;
 
-
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private ModelGenerator modelGenerator;
+
+    private JwtRequestPostProcessor token;
 
     private User testUser;
 
+
     @BeforeEach
     public void setUp() {
-        testUser = Instancio.of(User.class)
-                .ignore(Select.field(User::getId))
-                .supply(Select.field(User::getFirstName), () -> faker.name().firstName())
-                .supply(Select.field(User::getLastName), () -> faker.name().lastName())
-                .supply(Select.field(User::getEmail), () -> faker.internet().emailAddress())
-                .supply(Select.field(User::getPassword), () -> passwordEncoder.encode(faker.internet().password()))
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .defaultResponseCharacterEncoding(StandardCharsets.UTF_8)
+                .apply(springSecurity())
+                .build();
+
+        token = jwt().jwt(builder -> builder.subject("hexlet@example.com"));
+
+        testUser = Instancio.of(modelGenerator.getUserModel())
                 .create();
+        userRepository.save(testUser);
     }
 
     @Test
     public void testWelcome() throws Exception {
-        userRepository.save(testUser);
-
-        mockMvc.perform(get("/welcome"))
+        mockMvc.perform(get("/welcome").with(jwt()))
             .andExpect(status()
                     .isOk())
             .andExpect(content().string("Welcome to Spring"))
@@ -78,9 +92,11 @@ class AppApplicationTests {
 
     @Test
     public void testCreate() throws Exception {
+
         var dto = userMapper.map(testUser);
 
         var request = post("/api/users")
+                .with(token)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(dto));
 
@@ -99,12 +115,12 @@ class AppApplicationTests {
 
     @Test
     public void testUpdate() throws Exception {
-        userRepository.save(testUser);
 
         var dto = new UserUpdateDTO();
         dto.setEmail(JsonNullable.of(faker.internet().emailAddress()));
 
         var request = put("/api/users/" + testUser.getId())
+                .with(token)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(dto));
 
@@ -119,12 +135,13 @@ class AppApplicationTests {
 
     @Test
     public void testShow() throws Exception {
-        userRepository.save(testUser);
 
-        var request = get("/api/users/" + testUser.getId());
+        var request = get("/api/users/" + testUser.getId()).with(token);
+
         var result = mockMvc.perform(request)
                 .andExpect(status().isOk())
                 .andReturn();
+
         var body = result.getResponse().getContentAsString();
         assertThatJson(body).and(
                 v -> v.node("id").isEqualTo(testUser.getId()),
@@ -136,8 +153,7 @@ class AppApplicationTests {
 
     @Test
     public void testDelete() throws Exception {
-        userRepository.save(testUser);
-        var request = delete("/api/users/" + testUser.getId());
+        var request = delete("/api/users/" + testUser.getId()).with(token);
         mockMvc.perform(request)
                 .andExpect(status().isNoContent());
 
