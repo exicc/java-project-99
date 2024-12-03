cd5a5d5eda8290886fb1f3bc5dd6811af1b9ce46
exicc <ping.u@ya.ru>
2024-12-03 12:14:50 +0300

    Add filtration for tasks

--- src/main/java/hexlet/code/app/repository/TaskRepository.java
+++ src/main/java/hexlet/code/app/repository/TaskRepository.java
@@ -2,8 +2,9 @@ package hexlet.code.app.repository;
 
 import hexlet.code.app.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
     boolean existsByAssigneeId(Long userID);
     boolean existsByTaskStatusId(Long statusID);
     Task findByName(String name);
