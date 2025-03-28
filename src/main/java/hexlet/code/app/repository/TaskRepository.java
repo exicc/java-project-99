0d30d433673304331b84d04a5eaf78944fa2cd2c
Exicc <ping.u@ya.ru>
2025-03-28 21:43:21 +0300

    Fixes

--- src/main/java/hexlet/code/app/repository/TaskRepository.java
+++ src/main/java/hexlet/code/app/repository/TaskRepository.java
@@ -1,13 +1,20 @@
 package hexlet.code.app.repository;
 
 import hexlet.code.app.model.Task;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Task t WHERE t.assignee.id = :userId")
+    void deleteAllByAssigneeId(@Param("userId") Long userId);
     boolean existsByAssigneeId(Long userID);
     boolean existsByTaskStatusId(Long statusID);
     Task findByName(String name);
     Task findByIndex(Integer index);
-    void deleteAllByAssigneeId(Long userID);
 }
